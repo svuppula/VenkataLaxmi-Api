f5084d149feb9deda6c721265c6ceb512a5d9e3d
satish reddy vuppula <satishreddy.vuppula@gmail.com>
2021-02-28 01:17:07 -0500

    added properties

---
+++ src/main/java/com/venkatalaxmi/org/VenkataLaxmiApi/VenkataLaxmiApiApplication.java
@@ -0,0 +1,13 @@
+package com.venkatalaxmi.org.VenkataLaxmiApi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VenkataLaxmiApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VenkataLaxmiApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/venkatalaxmi/org/VenkataLaxmiApi/VenkataLaxmiApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.venkatalaxmi.org.VenkataLaxmiApi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VenkataLaxmiApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
